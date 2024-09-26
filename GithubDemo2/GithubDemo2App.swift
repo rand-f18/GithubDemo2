55bdcf0037beeb4d753e82868fde4fdd9c739861
Rand <randalobaid9@gmail.com>
2024-09-26 17:02:32 +0300

    Initial Commit

---
+++ GithubDemo2/GithubDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemo2App.swift
+//  GithubDemo2
+//
+//  Created by Rand Soliman Alobaid on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
